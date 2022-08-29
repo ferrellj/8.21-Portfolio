abb1b10fa97aaa88786743e49d420abac8f6b79b
ferrellj <justine.m.ferrell@gmail.com>
2022-08-29 13:27:35 -0500

    js menu

--- JS/index.js
+++ JS/index.js
@@ -18,6 +18,7 @@ function navigatecaseStudies(e) {
     behavior:"smooth"});
 } 
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
